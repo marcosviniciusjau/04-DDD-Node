37116fd8ce2424a4cd78191781bac1bb822005dd
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-10-15 13:58:09 -0300

    first commit

---
+++ src/domain/entities/answer.ts
@@ -0,0 +1,12 @@
+import { Entity } from "../../core/entities/entity"
+
+interface AnswerProps {
+  content: string
+  authorId: string
+  questionId: string
+}
+export class Answer extends Entity<AnswerProps> {
+  get content() {
+    return this.props.content
+  }
+}
---
+++ src/domain/entities/question.ts
@@ -0,0 +1,11 @@
+import { Entity } from "../../core/entities/entity"
+import { Slug } from "./value-objects/slug"
+
+interface QuestionProps {
+  title: string
+  content: string
+  slug: Slug
+  authorId: string
+}
+export class Question extends Entity<QuestionProps> {
+}
